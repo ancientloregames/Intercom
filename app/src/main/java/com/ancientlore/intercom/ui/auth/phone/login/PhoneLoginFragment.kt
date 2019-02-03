6846dfe13c5243b74980015090ee13b021b82bfe
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-02-03 18:26:21 +0200

    successfull phone auth handling

--- app/src/main/java/com/ancientlore/intercom/ui/auth/phone/login/PhoneLoginFragment.kt
+++ app/src/main/java/com/ancientlore/intercom/ui/auth/phone/login/PhoneLoginFragment.kt
@@ -3,6 +3,7 @@ package com.ancientlore.intercom.ui.auth.phone.login
 import android.view.View
 import com.ancientlore.intercom.R
 import com.ancientlore.intercom.backend.auth.PhoneAuthParams
+import com.ancientlore.intercom.backend.auth.User
 import com.ancientlore.intercom.databinding.PhoneLoginUiBinding
 import com.ancientlore.intercom.ui.auth.AuthFragment
 import com.ancientlore.intercom.ui.auth.phone.login.PhoneLoginViewModel.Companion.ERROR_NO_PHONE
@@ -36,11 +37,15 @@ class PhoneLoginFragment
 
 	override fun observeViewModel(viewModel: PhoneLoginViewModel) {
 		subscriptions.add(viewModel.observeLoginSuccessEvent()
-			.subscribe {  })
+			.subscribe { onLoginSuccess(it) })
 		subscriptions.add(viewModel.observeValidationRequestEvent()
 			.subscribe { onValidationRequest(it) })
 		subscriptions.add(viewModel.observeAlertRequestEvent()
 			.subscribe { showAlert(it) })
+	}
+
+	private fun onLoginSuccess(user: User) {
+		navigator?.onSuccessfullAuth(user)
 	}
 
 	private fun onValidationRequest(phone: PhoneAuthParams) {
