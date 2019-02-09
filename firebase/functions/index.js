5e87172642ae24f82a9de65ce9c7054db44c4044
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-02-09 09:13:27 +0200

    added onAddContact firestore cloud function

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -5,8 +5,24 @@ const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 
-exports.onUser = functions.auth.user().onCreate((user) => {
-  return db.ref('users/${user.uid}').set({
+exports.onNewUser = functions.auth.user().onCreate((user) => {
+  return admin.firestore().ref('users/${user.uid}').set({
     phoneNumber: user.phoneNumber
-  })
+  });
+});
+
+exports.onAddContact = functions.firestore
+  .document('users/{userId}/contacts/{contactId}')
+  .onCreate((contact, context) => {
+    const contactRef = contact.ref;
+    const contactPhone = contact.get('phone');
+    
+    const usersCollection = admin.firestore().collection('users');
+    return usersCollection.where('phone', '==', contactPhone).limit(1).get()
+      .then(users => {
+        users.forEach(user => {
+          if (user.exists)
+            contactRef.set({ uid: user.id }, { merge: true });
+        });
+      });
 });
