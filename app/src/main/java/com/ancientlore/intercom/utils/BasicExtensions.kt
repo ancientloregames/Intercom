5b1e6eab9e7c8ed05f9807d7c3463bd1a09b239a
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-02-10 11:22:18 +0200

    added getContacts extension for the contentResolver

--- app/src/main/java/com/ancientlore/intercom/utils/BasicExtensions.kt
+++ app/src/main/java/com/ancientlore/intercom/utils/BasicExtensions.kt
@@ -1,18 +1,52 @@
 package com.ancientlore.intercom.utils
 
+import android.Manifest
 import android.content.ContentResolver
+import android.content.Context
+import android.content.pm.PackageManager
 import android.provider.ContactsContract
+import androidx.annotation.RequiresPermission
+import androidx.core.app.ActivityCompat
+import com.ancientlore.intercom.data.model.Contact
 import java.io.Closeable
 import java.io.IOException
-import java.util.ArrayList
 
+fun Context.checkPermission(permission: String) = ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+
+@RequiresPermission(Manifest.permission.READ_CONTACTS)
+fun ContentResolver.getContacts(): List<Contact> {
+	val list = mutableListOf<Contact>()
+
+	val cursor = query(ContactsContract.Contacts.CONTENT_URI,
+		null, null, null, null)
+
+	if (cursor != null) {
+		try {
+			while (cursor.moveToNext()) {
+				val hasPhone = cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0
+				if (hasPhone) {
+					val id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+					val name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)) ?: ""
+					val phone = getContactPhones(id)[0] // TODO normalize phone number
+					list.add(Contact(name = name, phone = phone))
+				}
+			}
+		} finally {
+			cursor.safeClose()
+		}
+	}
+
+	return list
+}
+
+@RequiresPermission(Manifest.permission.READ_CONTACTS)
 fun ContentResolver.getContactPhones(contactId: String): List<String> {
+	val list = mutableListOf<String>()
+
 	val cursor = query(
 		ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
 		ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
 		arrayOf(contactId), null)
-
-	val list = ArrayList<String>()
 
 	if (cursor != null) {
 		try {
