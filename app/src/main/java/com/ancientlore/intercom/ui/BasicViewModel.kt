2dba744c96ac46caee6914e97da9ae51d3448b93
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-02-24 07:25:51 +0200

    added a toast event to the basic view model

--- app/src/main/java/com/ancientlore/intercom/ui/BasicViewModel.kt
+++ app/src/main/java/com/ancientlore/intercom/ui/BasicViewModel.kt
@@ -1,5 +1,12 @@
 package com.ancientlore.intercom.ui
 
 import androidx.lifecycle.ViewModel
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
 
-abstract class BasicViewModel : ViewModel()
+abstract class BasicViewModel : ViewModel() {
+
+	protected val toastRequest: PublishSubject<Int> = PublishSubject.create<Int>() // StringResId
+
+	fun observeToastRequest() = toastRequest as Observable<Int>
+}
