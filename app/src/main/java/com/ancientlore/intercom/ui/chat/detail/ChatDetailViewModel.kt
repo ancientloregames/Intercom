47699794b696d1fb99a1696c2a7cf01149cceb92
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-03-16 09:05:32 +0200

    chat flow: repository changes listening (working chatflow)

--- app/src/main/java/com/ancientlore/intercom/ui/chat/detail/ChatDetailViewModel.kt
+++ app/src/main/java/com/ancientlore/intercom/ui/chat/detail/ChatDetailViewModel.kt
@@ -2,10 +2,11 @@ package com.ancientlore.intercom.ui.chat.detail
 
 import androidx.databinding.ObservableField
 import com.ancientlore.intercom.App
-import com.ancientlore.intercom.backend.SimpleRequestCallback
+import com.ancientlore.intercom.backend.RequestCallback
 import com.ancientlore.intercom.data.model.Message
 import com.ancientlore.intercom.data.source.MessageRepository
 import com.ancientlore.intercom.ui.BasicViewModel
+import com.ancientlore.intercom.utils.Utils
 
 class ChatDetailViewModel(private val userId: String, chatId: String) : BasicViewModel() {
 
@@ -22,9 +23,14 @@ class ChatDetailViewModel(private val userId: String, chatId: String) : BasicVie
 		repository.setRemoteSource(dataSourceProvider.getMessageSource(chatId))
 	}
 
+	override fun onCleared() {
+		super.onCleared()
+		detachDataListener()
+	}
+
 	fun setListAdapter(listAdapter: ChatDetailAdapter) {
 		this.listAdapter = listAdapter
-		loadMessages()
+		attachDataListener()
 	}
 
 	fun onSendButtonClicked() {
@@ -35,15 +41,22 @@ class ChatDetailViewModel(private val userId: String, chatId: String) : BasicVie
 			}
 	}
 
-	private fun sendMessage(text: String) {
-		repository.addMessage(Message(senderId = userId, text = text), null)
-	}
-
-	private fun loadMessages() {
-		repository.getAll(object : SimpleRequestCallback<List<Message>>() {
+	private fun attachDataListener() {
+		repository.attachListener(object : RequestCallback<List<Message>>{
 			override fun onSuccess(result: List<Message>) {
 				listAdapter.setItems(result)
 			}
+			override fun onFailure(error: Throwable) {
+				Utils.logError(error)
+			}
 		})
+	}
+
+	private fun detachDataListener() {
+		repository.detachListener()
+	}
+
+	private fun sendMessage(text: String) {
+		repository.addMessage(Message(senderId = userId, text = text), null)
 	}
 }
