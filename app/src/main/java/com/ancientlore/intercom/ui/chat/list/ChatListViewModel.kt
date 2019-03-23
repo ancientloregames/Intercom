4c9ddbd0286f12561623a26b838c40d5984c53c1
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-03-23 09:41:25 +0200

    chat list: added beckend db listening

--- app/src/main/java/com/ancientlore/intercom/ui/chat/list/ChatListViewModel.kt
+++ app/src/main/java/com/ancientlore/intercom/ui/chat/list/ChatListViewModel.kt
@@ -5,6 +5,7 @@ import com.ancientlore.intercom.backend.SimpleRequestCallback
 import com.ancientlore.intercom.data.model.Chat
 import com.ancientlore.intercom.data.source.ChatRepository
 import com.ancientlore.intercom.ui.BasicViewModel
+import com.ancientlore.intercom.utils.Utils
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
 
@@ -15,12 +16,17 @@ class ChatListViewModel : BasicViewModel() {
 	private val contactListRequest = PublishSubject.create<Any>()
 	private val openChatByIdEvent = PublishSubject.create<String>() // Chat Id
 
+	override fun onCleared() {
+		super.onCleared()
+		detachDataListener()
+	}
+
 	fun setListAdapter(listAdapter: ChatListAdapter) {
 		this.listAdapter = listAdapter
 		listAdapter.setListener(object : ChatListAdapter.Listener {
 			override fun onChatSelected(chat: Chat) = openChatByIdEvent.onNext(chat.chatId)
 		})
-		loadChatList()
+		attachDataListener()
 	}
 
 	fun onShowContactListClicked() = contactListRequest.onNext(EmptyObject)
@@ -28,11 +34,18 @@ class ChatListViewModel : BasicViewModel() {
 	fun observeContactListRequest() = contactListRequest as Observable<*>
 	fun observeChatOpenById() = openChatByIdEvent as Observable<String>
 
-	private fun loadChatList() {
-		ChatRepository.getAll(object : SimpleRequestCallback<List<Chat>>() {
+	private fun attachDataListener() {
+		ChatRepository.attachListener(object : SimpleRequestCallback<List<Chat>>() {
 			override fun onSuccess(result: List<Chat>) {
 				listAdapter.setItems(result)
 			}
+			override fun onFailure(error: Throwable) {
+				Utils.logError(error)
+			}
 		})
+	}
+
+	private fun detachDataListener() {
+		ChatRepository.detachListener()
 	}
 }
