4f56f8be9f0ee7c0da222935cc5a600fedae5a03
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-06-30 06:38:40 +0300

    contacts: added lastSeenTime field

--- app/src/main/java/com/ancientlore/intercom/data/model/Contact.kt
+++ app/src/main/java/com/ancientlore/intercom/data/model/Contact.kt
@@ -1,5 +1,18 @@
 package com.ancientlore.intercom.data.model
 
+import com.google.firebase.firestore.Exclude
+import java.text.DateFormat
+import java.util.*
+
 data class Contact(val phone: String = "",
                    val name: String = "",
-                   val chatId: String = "")
+                   val chatId: String = "",
+                   val lastSeenTime: Long = 0) {
+
+  companion object {
+    private val dateFormat = DateFormat.getTimeInstance(DateFormat.SHORT)
+  }
+
+  @delegate:Exclude
+  val lastSeenDate: String by lazy { dateFormat.format(Date(lastSeenTime)) }
+}
