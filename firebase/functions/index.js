8f234d5a57d1389b942c1d317850b5950d74755d
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-07-26 18:17:45 +0300

    firebase: onCreateChat, onCreateMessage refactor

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -5,6 +5,9 @@ const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 
+const chats = admin.firestore().collection('chats');
+const users = admin.firestore().collection('users');
+
 exports.onNewUser = functions.auth.user().onCreate(user => {
   const usersCollection = admin.firestore().collection('users');
   return usersCollection.doc(user.phoneNumber).set({
@@ -12,27 +15,65 @@ exports.onNewUser = functions.auth.user().onCreate(user => {
   });
 });
 
-exports.onCreateDialog = functions.firestore
-  .document('users/{userId}/dialogs/{dialogId}')
-  .onCreate((dialog, context) => {
-    const dialogId = context.params.dialogId;
-    const userId = context.params.userId;
-    const recipientId = dialog.get('recipientId');
-    
-    const usersCollection = admin.firestore().collection('users');
-    usersCollection.doc(`${userId}/contacts/${recipientId}`).set({
-      'chatId': dialogId
-    }, { merge: true });
-    return usersCollection.doc(`${recipientId}/contacts/${userId}`).set({
-      'phone': userId,
-      'chatId': dialogId
-    }, { merge: true });
+exports.onCreateChat = functions.firestore
+  .document('chats/{chatId}')
+  .onCreate((chat, context) => {
+    const chatId = context.params.chatId;
+    const chatName = chat.get('name');
+
+    // dialog chat
+    if (chatName === '') {
+      const initiatorId = chat.get('initiatorId');
+      const recipientId = chat.get('participants').find(userId => userId != initiatorId);
+      users.doc(recipientId).collection('contacts').doc(initiatorId).get(snap => {
+        const contactName = snap.get('name');
+        users.doc(recipientId).collection('chats').doc(chatId).set({
+          'id': chatId,
+          'name': contactName
+        }, { merge: true });
+        snap.ref.set({
+          'chatId': chatId
+        }, { merge: true });
+      });
+      return users.doc(initiatorId).collection('contacts').doc(recipientId).get(snap => {
+        const contactName = snap.get('name');
+        users.doc(initiatorId).collection('chats').doc(chatId).set({
+          'id': chatId,
+          'name': contactName
+        }, { merge: true });
+        snap.ref.set({
+          'chatId': chatId
+        }, { merge: true });
+      });
+    } 
+    // group chat
+    else {
+      return chat.get('participants').forEach(userId => {
+          users.doc(userId).collection('chats').doc(chatId).set({
+            'id': chatId,
+            'name': chatName
+          }, { merge: true });
+      });
+    }
 });
 
-exports.onNewMessage = functions.firestore
+exports.onCreateMessage = functions.firestore
   .document('chats/{chatId}/messages/{messageId}')
   .onCreate((message, context) => {
-    return message.ref.set({
-      'timestamp': Date.now()
-    }, { merge: true })
+    const timestamp = Date.now()
+
+    message.ref.set({
+      'timestamp': timestamp
+    }, { merge: true });
+
+    const text = message.get('text')
+    const chatId = context.params.chatId;
+    return chats.doc(chatId).get().then(snap => {
+      snap.get('participants').forEach(userId => {
+        users.doc(userId).collection('chats').doc(chatId).set({
+          'lastMsgTime': timestamp,
+          'lastMsgText': text
+        }, { merge: true })
+      });
+    });
 });
