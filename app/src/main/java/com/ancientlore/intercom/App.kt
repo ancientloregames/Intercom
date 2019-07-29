7a66e63f4502619c325c0c726a25f810afa6e172
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-07-29 19:20:58 +0300

    enabled the vector drawables support for the older android versions

--- app/src/main/java/com/ancientlore/intercom/App.kt
+++ app/src/main/java/com/ancientlore/intercom/App.kt
@@ -1,11 +1,16 @@
 package com.ancientlore.intercom
 
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDexApplication
 import com.ancientlore.intercom.backend.firebase.FirebaseFactory
 
 class App : MultiDexApplication() {
 
 	companion object {
+
+		init {
+			AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+		}
 
 		val backend = FirebaseFactory
 	}
