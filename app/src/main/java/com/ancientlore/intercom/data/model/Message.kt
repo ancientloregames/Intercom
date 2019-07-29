926d767e864a69c92d390ad1cdd0b1715c4be856
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-07-29 18:51:21 +0300

    added an id and status fields for the message model

--- app/src/main/java/com/ancientlore/intercom/data/model/Message.kt
+++ app/src/main/java/com/ancientlore/intercom/data/model/Message.kt
@@ -1,31 +1,42 @@
 package com.ancientlore.intercom.data.model
 
 import android.net.Uri
+import androidx.annotation.IntDef
 import androidx.annotation.StringDef
 import com.google.firebase.firestore.Exclude
 import java.text.DateFormat
 import java.text.DateFormat.SHORT
 
-data class Message(val timestamp: Long = System.currentTimeMillis(),// TODO get server timestamp on app start?
+data class Message(val id: String = "",
+                   val timestamp: Long = System.currentTimeMillis(),// TODO get server timestamp on app start?
                    val senderId: String = "",
                    val text: String = "",
                    val info: String = "",
                    val attachUrl: String = "",
-                   @Type val type: String = TYPE_TEXT)
+                   @Type val type: String = TYPE_TEXT,
+                   @Status val status: Int = STATUS_WAIT)
 	: Comparable<Message> {
 
-  companion object {
+	companion object {
     const val TYPE_TEXT = "text"
     const val TYPE_IMAGE = "image"
     const val TYPE_FILE = "file"
+
+	  const val STATUS_WAIT = 0
+	  const val STATUS_SENT = 1
+	  const val STATUS_RECEIVED = 2
   }
 
   constructor(senderId: String, fileData: FileData) : this(
-    0, senderId, fileData.name, fileData.getInfo(), fileData.uri.toString(), TYPE_FILE)
+	  "", 0, senderId, fileData.name, fileData.getInfo(), fileData.uri.toString(), TYPE_FILE)
 
-  @StringDef(TYPE_TEXT, TYPE_IMAGE)
+	@StringDef(TYPE_TEXT, TYPE_IMAGE)
   @Retention(AnnotationRetention.SOURCE)
   annotation class Type
+
+	@IntDef(STATUS_WAIT, STATUS_SENT, STATUS_RECEIVED)
+	@Retention(AnnotationRetention.SOURCE)
+	annotation class Status
 
   @delegate:Exclude @get:Exclude
   val attachUri: Uri by lazy { Uri.parse(attachUrl) }
