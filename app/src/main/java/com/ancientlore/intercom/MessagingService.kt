42c92ddff2f4342421ade23dae635fc795854917
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-08-10 19:56:47 +0300

    messaging service

---
+++ app/src/main/java/com/ancientlore/intercom/MessagingService.kt
@@ -0,0 +1,37 @@
+package com.ancientlore.intercom
+
+import com.ancientlore.intercom.backend.SimpleRequestCallback
+import com.ancientlore.intercom.data.model.PushMessage
+import com.ancientlore.intercom.data.source.UserRepository
+import com.ancientlore.intercom.utils.JsonUtils
+import com.ancientlore.intercom.utils.NotificationManager
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MessagingService: FirebaseMessagingService() {
+
+	private val manager: NotificationManager = NotificationManager.getInstance(this)
+
+	override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+		super.onMessageReceived(remoteMessage)
+
+		remoteMessage?.data?.let { data ->
+			val message = JsonUtils.deserialize(data, PushMessage::class.java)
+			manager.showNotification(message)
+		}
+	}
+
+	override fun onNewToken(newToken: String?) {
+		newToken?.let { token ->
+			App.backend.getAuthManager().getCurrentUser()
+				?.let { user ->
+					if (!UserRepository.hasRemoteSource()) {
+						val remoteSource = App.backend.getDataSourceProvider().getUserSource(user.id)
+						UserRepository.setRemoteSource(remoteSource)
+					}
+
+					UserRepository.updateNotificationToken(token, object : SimpleRequestCallback<Any>() {})
+				}
+		}
+	}
+}
