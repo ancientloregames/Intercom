a4d4f3a3b30c8d0e2a5599e6890d0e87d0ec9fad
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-08-30 18:32:45 +0300

    integrated Input Panel Manager to the chat flow viewModel

--- app/src/main/java/com/ancientlore/intercom/ui/chat/flow/ChatFlowViewModel.kt
+++ app/src/main/java/com/ancientlore/intercom/ui/chat/flow/ChatFlowViewModel.kt
@@ -1,19 +1,24 @@
 package com.ancientlore.intercom.ui.chat.flow
 
+import android.media.MediaRecorder
 import android.net.Uri
+import android.util.Log
 import androidx.databinding.ObservableField
 import com.ancientlore.intercom.App
 import com.ancientlore.intercom.EmptyObject
-import com.ancientlore.intercom.R
 import com.ancientlore.intercom.backend.ProgressRequestCallback
 import com.ancientlore.intercom.backend.RequestCallback
+import com.ancientlore.intercom.backend.SimpleRequestCallback
 import com.ancientlore.intercom.data.model.FileData
 import com.ancientlore.intercom.data.model.Message
 import com.ancientlore.intercom.data.source.MessageRepository
 import com.ancientlore.intercom.ui.BasicViewModel
 import com.ancientlore.intercom.utils.Utils
+import com.ancientlore.intercom.utils.extensions.createAudioMessageFile
+import com.ancientlore.intercom.view.MessageInputManager
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
+import java.io.File
 
 class ChatFlowViewModel(private val userId: String,
                         private val chatId: String)
@@ -28,6 +33,60 @@ class ChatFlowViewModel(private val userId: String,
 	private val messageText get() = textField.get()!!
 
 	private val openAttachMenuSubj = PublishSubject.create<Any>()
+
+	private val recordAudioSubj = PublishSubject.create<Any>()
+
+	private var inputManager: MessageInputManager? = null
+
+	fun attachInputPanelManager(manager: MessageInputManager) {
+		inputManager = manager
+		manager.setListener(object : MessageInputManager.Listener {
+			private var recorder: MediaRecorder? = null
+
+			private var outFile: File? = null
+
+			override fun onStarted() {
+				outFile = App.context.createAudioMessageFile()
+				outFile?.let { file ->
+					try {
+						recorder = MediaRecorder().apply {
+							setAudioSource(MediaRecorder.AudioSource.MIC)
+							setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+							setOutputFile(file.absolutePath)
+							setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+							prepare()
+							start()
+						}
+					} catch (e: IllegalStateException) {
+						e.printStackTrace()
+					}
+				} ?: Log.e("ChatFlow", "Unable to create audio output file")
+			}
+
+			override fun onCompleted() {
+				outFile?.let { file ->
+					releaseRecorder()
+				}
+			}
+
+			override fun onCanceled() {
+				releaseRecorder()
+				outFile?.delete()
+			}
+
+			private fun releaseRecorder() {
+				if (recorder != null) {
+					try {
+						recorder!!.stop()
+						recorder!!.release()
+						recorder = null
+					} catch (e: IllegalStateException) {
+						e.printStackTrace()
+					}
+				}
+			}
+		})
+	}
 
 	init {
 		val dataSourceProvider = App.backend.getDataSourceProvider()
@@ -79,58 +138,49 @@ class ChatFlowViewModel(private val userId: String,
 			}
 	}
 
+	fun onRecordButtonClicked() = recordAudioSubj.onNext(EmptyObject)
+
+	fun onStopRecordButtonClicked() {
+		inputManager?.onStop()
+	}
+
+	fun onCancelRecordButtonClicked() {
+		inputManager?.onCancel()
+	}
+
 	private fun sendMessage(text: String) {
 		repository.addMessage(Message(senderId = userId, text = text), null)
 	}
 
 	fun handleAttachedImage(fileData: FileData, conpressed: Uri) {
 		val message = Message(senderId = userId, attachUrl = conpressed.toString(), type = Message.TYPE_IMAGE)
-		repository.addMessage(message, object : RequestCallback<String> {
+		repository.addMessage(message, object : SimpleRequestCallback<String>() {
 			override fun onSuccess(messageId: String) {
 				App.backend.getStorageManager().uploadImage(fileData, chatId, object : ProgressRequestCallback<Uri> {
 					override fun onProgress(progress: Int) {
 						listAdapter.setFileUploadProgress(messageId, progress)
 					}
 					override fun onSuccess(uri: Uri) {
-						repository.updateMessageUri(messageId, uri, object : RequestCallback<Any> {
-							override fun onSuccess(result: Any) {
-								toastRequest.onNext(R.string.success) // TODO remove this callback on release
-							}
-							override fun onFailure(error: Throwable) {
-								toastRequest.onNext(R.string.error)
-								Utils.logError(error)
-							}
-						})
+						repository.updateMessageUri(messageId, uri, object : SimpleRequestCallback<Any>() {})
 					}
 					override fun onFailure(error: Throwable) {
 						Utils.logError(error)
 					}
 				})
 			}
-			override fun onFailure(error: Throwable) {
-				Utils.logError(error)
-			}
 		})
 	}
 
 	fun handleAttachedFile(fileData: FileData) {
 		val message = Message(userId, fileData)
-		repository.addMessage(message, object : RequestCallback<String> {
+		repository.addMessage(message, object : SimpleRequestCallback<String>() {
 			override fun onSuccess(messageId: String) {
 				App.backend.getStorageManager().uploadFile(fileData, chatId, object : ProgressRequestCallback<Uri> {
 					override fun onProgress(progress: Int) {
 						listAdapter.setFileUploadProgress(messageId, progress)
 					}
 					override fun onSuccess(result: Uri) {
-						repository.updateMessageUri(messageId, result, object : RequestCallback<Any> {
-							override fun onSuccess(result: Any) {
-								toastRequest.onNext(R.string.success) // TODO remove this callback on release
-							}
-							override fun onFailure(error: Throwable) {
-								toastRequest.onNext(R.string.error)
-								Utils.logError(error)
-							}
-						})
+						repository.updateMessageUri(messageId, result, object : SimpleRequestCallback<Any>() {})
 					}
 					override fun onFailure(error: Throwable) {
 						Utils.logError(error)
@@ -148,4 +198,6 @@ class ChatFlowViewModel(private val userId: String,
 	}
 
 	fun observeAttachMenuOpen() = openAttachMenuSubj as Observable<Any>
+
+	fun observeAudioRecord() = recordAudioSubj as Observable<Any>
 }
