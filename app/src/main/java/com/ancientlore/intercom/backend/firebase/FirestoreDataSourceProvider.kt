e31fe2fabe1cab5816797da140bb110259ab6e74
nickolas <ancientloregames@gmail.com>
2021-01-30 14:27:43 +0200

    moved to non-fcm firebase data sources

--- app/src/main/java/com/ancientlore/intercom/backend/firebase/FirestoreDataSourceProvider.kt
+++ app/src/main/java/com/ancientlore/intercom/backend/firebase/FirestoreDataSourceProvider.kt
@@ -1,19 +1,16 @@
 package com.ancientlore.intercom.backend.firebase
 
 import com.ancientlore.intercom.backend.DataSourceProvider
-import com.ancientlore.intercom.data.source.remote.firestore.FirestoreChatSource
-import com.ancientlore.intercom.data.source.remote.firestore.FirestoreContactSource
-import com.ancientlore.intercom.data.source.remote.firestore.FirestoreMessageSource
-import com.ancientlore.intercom.data.source.remote.firestore.FirestoreUserSource
+import com.ancientlore.intercom.data.source.remote.firestore.*
 
 object FirestoreDataSourceProvider
 	: DataSourceProvider {
 
 	override fun getUserSource(userId: String) = FirestoreUserSource(userId)
 
-	override fun getChatSource(userId: String) = FirestoreChatSource.getInstance(userId)
+	override fun getChatSource(userId: String) = FirestoreChatSourceNoCF.getInstance(userId)
 
-	override fun getMessageSource(chatId: String) = FirestoreMessageSource(chatId)
+	override fun getMessageSource(chatId: String) = FirestoreMessageNoCF(chatId)
 
 	override fun getContactSource(userId: String) = FirestoreContactSource.getInstance(userId)
 }
