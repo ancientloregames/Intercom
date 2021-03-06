292c04b4098507a05ba49f1f5d53e4349c7da8a1
nickolas <ancientloregames@gmail.com>
2021-03-06 18:43:52 +0200

    missed changes

--- app/src/main/java/com/ancientlore/intercom/ui/chat/list/ChatListFragment.kt
+++ app/src/main/java/com/ancientlore/intercom/ui/chat/list/ChatListFragment.kt
@@ -1,6 +1,8 @@
 package com.ancientlore.intercom.ui.chat.list
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.SearchView
 import com.ancientlore.intercom.R
@@ -16,6 +18,27 @@ class ChatListFragment : BasicFragment<ChatListViewModel, ChatListUiBinding>() {
 		fun newInstance() = ChatListFragment()
 	}
 
+	private val menuCallback = Runnable1<Menu> { menu ->
+		activity?.menuInflater?.inflate(R.menu.chat_list_menu, menu)
+
+		val search = menu.findItem(R.id.search).actionView as SearchView
+		search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+			override fun onQueryTextSubmit(query: String?): Boolean {
+				query?.let { constraint ->
+					viewModel.filter(constraint)
+				}
+				return true
+			}
+			override fun onQueryTextChange(newText: String?): Boolean {
+				newText
+					?.takeIf { it.length > 1 }
+					?.let { viewModel.filter(it) }
+					?:run { viewModel.filter("") }
+				return true
+			}
+		})
+	}
+
 	override fun getLayoutResId() = R.layout.chat_list_ui
 
 	override fun createViewModel() = ChatListViewModel()
@@ -23,33 +46,6 @@ class ChatListFragment : BasicFragment<ChatListViewModel, ChatListUiBinding>() {
 	override fun bind(view: View, viewModel: ChatListViewModel) {
 		dataBinding = ChatListUiBinding.bind(view)
 		dataBinding.ui = viewModel
-	}
-
-	override fun initView(view: View, savedInstanceState: Bundle?) {
-		initToolbarMenu()
-		listView.adapter = ChatListAdapter(context!!, mutableListOf())
-	}
-
-	private fun initToolbarMenu() {
-		navigator?.createToolbarMenu(toolbar, Runnable1 { menu ->
-			activity?.menuInflater?.inflate(R.menu.chat_list_menu, menu)
-			val search = menu.findItem(R.id.search).actionView as SearchView
-			search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-				override fun onQueryTextSubmit(query: String?): Boolean {
-					query?.let { constraint ->
-						viewModel.filter(constraint)
-					}
-					return true
-				}
-				override fun onQueryTextChange(newText: String?): Boolean {
-					newText
-						?.takeIf { it.length > 1 }
-						?.let { viewModel.filter(it) }
-						?:run { viewModel.filter("") }
-					return true
-				}
-			})
-		})
 	}
 
 	override fun initViewModel(viewModel: ChatListViewModel) {
@@ -61,6 +57,22 @@ class ChatListFragment : BasicFragment<ChatListViewModel, ChatListUiBinding>() {
 			.subscribe { openContactList() })
 		subscriptions.add(viewModel.observeChatOpen()
 			.subscribe { openChatFlow(it) })
+	}
+
+	override fun initView(view: View, savedInstanceState: Bundle?) {
+		navigator?.createToolbarMenu(toolbar, menuCallback)
+		setHasOptionsMenu(true)
+		listView.adapter = ChatListAdapter(requireContext(), mutableListOf())
+	}
+
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+		return when (item.itemId) {
+			R.id.settings -> {
+				navigator?.openSettings()
+				true
+			}
+			else -> super.onOptionsItemSelected(item)
+		}
 	}
 
 	private fun openContactList() = navigator?.openContactList()
