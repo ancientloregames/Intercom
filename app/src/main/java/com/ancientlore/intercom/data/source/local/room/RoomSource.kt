71e8f1acffa3036189ab159f790a377bdc4064a1
nickolas <ancientloregames@gmail.com>
2021-08-19 14:52:24 +0300

    source: catch of possible executor thread exceptions

--- app/src/main/java/com/ancientlore/intercom/data/source/local/room/RoomSource.kt
+++ app/src/main/java/com/ancientlore/intercom/data/source/local/room/RoomSource.kt
@@ -1,11 +1,22 @@
 package com.ancientlore.intercom.data.source.local.room
 
+import com.ancientlore.intercom.utils.Utils
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 abstract class RoomSource {
 
-	private val service: ExecutorService = Executors.newSingleThreadExecutor { r -> Thread(r, getWorkerThreadName()) }
+	private val service: ExecutorService = Executors.newSingleThreadExecutor { r ->
+		object : Thread(r, getWorkerThreadName()) {
+			override fun run() {
+				try {
+					super.run()
+				} catch (e: Throwable) {
+					Utils.logError(e)
+				}
+			}
+		}
+	}
 
 	protected abstract fun getWorkerThreadName(): String
 
--- app/src/main/java/com/ancientlore/intercom/data/source/remote/firestore/FirestoreSource.kt
+++ app/src/main/java/com/ancientlore/intercom/data/source/remote/firestore/FirestoreSource.kt
@@ -1,14 +1,26 @@
 package com.ancientlore.intercom.data.source.remote.firestore
 
+import com.ancientlore.intercom.utils.Utils
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.google.firebase.firestore.QuerySnapshot
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 abstract class FirestoreSource<T> {
 
-	private val service: ExecutorService = Executors.newSingleThreadExecutor { r -> Thread(r, getWorkerThreadName()) }
+	private val service: ExecutorService = Executors.newSingleThreadExecutor { r ->
+		object : Thread(r, getWorkerThreadName()) {
+			override fun run() {
+				try {
+					super.run()
+				} catch (e: Throwable) {
+					Utils.logError(e)
+				}
+			}
+		}
+	}
 
 	protected val db = FirebaseFirestore.getInstance()
 
@@ -19,6 +31,8 @@ abstract class FirestoreSource<T> {
 	protected fun deserialize(snapshot: DocumentSnapshot): T? = snapshot.toObject(getObjectClass())
 
 	protected fun deserialize(snapshot: QuerySnapshot): List<T> = snapshot.toObjects(getObjectClass())
+
+	protected fun deserialize(snapshot: QueryDocumentSnapshot): T = snapshot.toObject(getObjectClass())
 
 	protected fun exec(command: Runnable) {
 		service.execute(command)
